8c3b081d10b8a7609c4b393caf9b9843a767ccb6
NatanaelVineDjapri <natanaelvinedj@gmail.com>
2025-05-24 13:52:26 +0700

    Add forgot password form and related error messages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthSessionController;
+use App\Http\Controllers\ForgetPasswordController;
 
-require __DIR__.'/auth.php';
+
+
+Route::get('/auth', [AuthSessionController::class, 'formLogin'])->name('login');
+Route::post('/login', [AuthSessionController::class, 'manualLogin']);
+Route::post('/register', [AuthSessionController::class, 'register']);
+Route::get('/forget-password',[ForgetPasswordController::class,'formForgetPassword']);
+Route::post('/forget-password',[ForgetPasswordController::class,'submitForgetPassForm']);
+
+Route::post('/logout', [AuthSessionController::class, 'logout'])->name('logout');
+
+
