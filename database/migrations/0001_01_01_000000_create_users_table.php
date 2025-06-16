b24f9ce92ce77a2a75238943addbdd6e685d6179
NatanaelVineDjapri <natanaelvinedj@gmail.com>
2025-06-16 20:13:29 +0700

    Delete Email Verif

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration {
             $table->text('avatar')->nullable();
             $table->text('banner')->nullable();
             $table->string('description')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
             $table->date('dob')->nullable(); 
             $table->rememberToken();
             $table->timestamps();
