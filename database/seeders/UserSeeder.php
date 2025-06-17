ec9186d8f635951b8897ba0b8cef4b86bec5f95c
RyanPrasetyaa <yiyan125@gmail.com>
2025-06-17 09:44:10 +0700

    Adding seeders for retweets

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Tweet;
 use App\Models\Comment;
 use App\Models\Like;
+use App\Models\Retweet;
 
 class UserSeeder extends Seeder
 {
@@ -16,30 +17,48 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {       
-            User::factory()->count(500)->create()->each(function($user){
+        User::factory()->count(500)->create()->each(function($user)
+        {
             Tweet::factory(rand(5,20))->create(['user_id'=>$user->id]);
 
             $allTweets = Tweet::all();
+
             $likeCount = min(rand(15, 150), $allTweets->count());
+
             $randomTweet = $allTweets->random($likeCount)->pluck('id');
+
             foreach ($randomTweet as $tweetId) {
                 Like::factory()->create([
                     'user_id' => $user->id,
                     'tweet_id' => $tweetId,
                 ]);
             }
+
             $commentCount = min(rand(10, 200), $allTweets->count());
+
             $commentTweet = $allTweets->random($commentCount)->pluck('id');
+
             foreach($commentTweet as $tweetId){
                 Comment::factory()->create([
                     'user_id'=>$user->id,
                     'tweet_id'=>$tweetId,
                 ]);
             }
+
             $user->following()->attach(
                 User::inRandomOrder()->where('id', '!=', $user->id)->take(rand(10, 100))->pluck('id')
             );
-    
+            
+            $retweetCount = min(rand(15, 150), $allTweets->count());
+
+            $randomTweet = $allTweets->random($retweetCount)->pluck('id');
+
+            foreach ($randomTweet as $tweetId) {
+                Retweet::factory()->create([
+                    'user_id' => $user->id,
+                    'tweet_id' => $tweetId,
+                ]);
+            }
          });
     }
 }
---
+++ storage/framework/views/351553b24eea04f4099d0af5015d0ad1.php
@@ -0,0 +1,99 @@
+ 
+
+<?php $__env->startSection('head'); ?>
+    <title>Profile | Pingy</title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/styleProfileEdit.css')); ?>">
+    <?php echo $__env->yieldContent('head'); ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="edit-profile-wrapper">
+        <div class="edit-profile-card">
+            <div class="edit-profile-header">Edit Profile</div>
+            <div class="edit-profile-body">
+                <form method="POST" action="<?php echo e(route('updateprofile', $user->id)); ?>" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('PATCH'); ?>
+                    <div class="form-group">
+                        <label for="name" class="label">Name</label>
+                        <input id="name" type="text" class="input" name="name" value="<?php echo e(old('name', $user->name)); ?>" required>
+                        <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="error"><?php echo e($message); ?></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="username" class="label">Username</label>
+                        <input id="username" type="text" class="input" name="username" value="<?php echo e(old('username', $user->username)); ?>" required>
+                        <?php $__errorArgs = ['username'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="error"><?php echo e($message); ?></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="label">Bio</label>
+                        <textarea id="description" class="input" name="description" rows="3"><?php echo e(old('description', $user->description)); ?></textarea>
+                        <?php $__errorArgs = ['description'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="error"><?php echo e($message); ?></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="avatar" class="label">Profile Image</label>
+                        <input id="avatar" type="file" class="input" name="avatar">
+                        <?php $__errorArgs = ['avatar'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="error"><?php echo e($message); ?></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="banner" class="label">Banner Image</label>
+                        <input id="banner" type="file" class="input" name="banner">
+                        <?php $__errorArgs = ['banner'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="error"><?php echo e($message); ?></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn">Update Profile</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\New folder\htdocs\ProjectUAS\Pingy\resources\views/profiles/profile-edit.blade.php ENDPATH**/ ?>
